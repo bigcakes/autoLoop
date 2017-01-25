ea7c53ab0e1ec22c3fee3e5d7ccd534e39ca208f
John Wiegert <jjjsgmw@hotmail.com>
2017-01-25 15:21:42 -0600

    Code cleanup and storing the selected options in the URL.

--- js/index.js
+++ js/index.js
@@ -1,34 +1,63 @@
+(function ($, window, document, undefined){
+  // Load the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// Load the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+  var player,
+    startTime,
+    endTime,
+    videoId,
+    loopingTimeout;
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// Replace the 'ytplayer' element with an <iframe> and
-// YouTube player after the API code downloads.
-var player,
-  startTime,
-  endTime,
-  videoId,
-  loopingTimeout;
-
-function onYouTubeIframeAPIReady() {
-  $("#loadVideo").click(function () {
-    startTime = +$(".start-time").val();
-    endTime = +$(".end-time").val();
-
-    if (startTime > endTime) {
-      alert("No!!");
-      return;
+  window.addEventListener('popstate', function(event) {
+    if (event.state) {
+      loadNewPlayer(event.state);
+    } else {
+      loadFromCurrentURL();
     }
+  });
 
-    var videoId = $(".video-link").val().split('v=')[1];
-    var ampersandPosition = videoId.indexOf('&');
-    if(ampersandPosition != -1) {
-      videoId = videoId.substring(0, ampersandPosition);
+  function loadFromCurrentURL() {
+    var urlVideoId = getParameterByName("videoId", window.location);
+    var urlStartTime = getParameterByName("startTime", window.location);
+    var urlEndTime = getParameterByName("endTime", window.location);
+
+    if (urlVideoId && urlStartTime && urlEndTime) {
+      loadNewPlayer({ startTime: urlStartTime, endTime: urlEndTime, videoId: urlVideoId })
     }
+  }
+
+  window.onYouTubeIframeAPIReady = function () {
+    loadFromCurrentURL();
+    $("#loadVideo").click(function () {
+      startTime = +$(".start-time").val();
+      endTime = +$(".end-time").val();
+
+      if (startTime > endTime) {
+        alert("No!!");
+        return;
+      }
+
+      var tempVideoId = getParameterByName("v", $(".video-link").val());
+      var data = { startTime: startTime, endTime: endTime, videoId: tempVideoId };
+      loadNewPlayer(data);
+      window.history.pushState(data, "Video - " + tempVideoId, "?videoId=" + tempVideoId + "&startTime=" + startTime + "&endTime=" + endTime);
+    });
+  }
+
+  function loadNewPlayer(data) {
+    $("#loadVideo i").addClass("fa-spinner fa-pulse");
+    $("#loadVideo i").removeClass("fa-play");
+    startTime = +data.startTime;
+    endTime = +data.endTime;
+    videoId = data.videoId;
+
+    $(".start-time").val(data.startTime);
+    $(".end-time").val(data.endTime);
+    $(".video-link").val("https://www.youtube.com/watch?v=" + data.videoId);
+    document.title = "AutoLoop - " + data.videoId;
 
     if (player) {
       player.destroy();
@@ -39,30 +68,47 @@ function onYouTubeIframeAPIReady() {
     }
     
     player = new YT.Player('videoPlayer', {
-      videoId: videoId,
+      videoId: data.videoId,
       events: {
         'onReady': onPlayerReady
+      },
+      //TODO: Do we need this?
+      playerVars: {
+        controls: 0
       }
     });
-  });
-}
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-  player.seekTo(startTime);
-  loopingTimeout = setTimeout(loop, 1000);
-}
-
-function loop() {
-  var timePassed = player.getCurrentTime();
-
-  console.log("looping", new Date(), "time", timePassed)
-
-  if (timePassed > endTime) {
-
-    player.seekTo(startTime);
   }
 
-  loopingTimeout = setTimeout(loop, 1000);
-}
+  function onPlayerReady(event) {
+    event.target.playVideo();
+    player.seekTo(startTime);
+    loopingTimeout = setTimeout(loop, 1000);
+    $("#loadVideo i").removeClass("fa-spinner fa-pulse");
+    $("#loadVideo i").addClass("fa-play");
+  }
+
+  function loop() {
+    var timePassed = player.getCurrentTime();
+
+    //console.log("looping", new Date(), "time", timePassed)
+
+    if (timePassed > endTime) {
+
+      player.seekTo(startTime);
+    }
+
+    loopingTimeout = setTimeout(loop, 1000);
+  }
+
+  function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+})(jQuery, window, document);
